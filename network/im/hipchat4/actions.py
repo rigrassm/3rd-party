2bf00d3427d09cecce8bd2330f69e64ce13aa23c
Ricky Grassmuck <rigrassm@gmail.com>
2017-07-17 19:00:59 -0500

    Fix issue moving icon files

--- network/im/hipchat4/actions.py
+++ network/im/hipchat4/actions.py
@@ -7,6 +7,20 @@ WorkDir = "."
 
 
 def install():
+    icon_sizes = list(
+            "16x16", 
+            "24x24", 
+            "32x32", 
+            "48x48",
+            "128x128",
+            "256x256", 
+            "512x512",
+            "1024x1024",
+            )
     pisitools.insinto("/opt/hipchat", "hipchat4-4.29.4.1662/HipChat4/*")
-    pisitools.insinto("/usr/share/icons/hicolor", "hipchat4-4.29.4.1662/icons/hicolor/*")
+    for i in icon_sizes:
+        pisitools.domove(
+                "hipchat4-4.29.4.1662/icons/hicolor/" + i + "/apps/*", 
+                "/usr/share/icons/hicolor/" + i + "/apps/"
+                )
     pisitools.dosym("/opt/hipchat/bin/HipChat4", "/usr/bin/hipchat")
